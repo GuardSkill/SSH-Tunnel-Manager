6d76a84d4db19420056bd68eeb1c51ecc20d259d
GuardSkill <497291093@qq.com>
2025-10-29 19:51:50 +0800

    编译加入图标

--- build_windows.bat
+++ build_windows.bat
@@ -15,10 +15,12 @@ echo Building executable...
 python -m PyInstaller --name=SSHTunnelManager --windowed --onefile --clean ^
     --collect-all PyQt5 ^
     --hidden-import PyQt5.sip ^
+    --icon resources\app_icon.ico ^
     ssh_tunnel_manage_v2.py
 @REM python -m PyInstaller --name=SSHTunnelManager --windowed --onefile --clean ^
 @REM     --collect-all PyQt5 ^
 @REM     --hidden-import PyQt5.sip ^
+@REM     --icon resources\app_icon.ico ^
 @REM     ssh_tunnel_manage_v1.py
 
 
